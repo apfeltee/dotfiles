4fbf011251ef6bced4417a1e37a33b4175a8d1ee
John Doe <john@doe.nb>
2014-11-18 19:05:43 +0100

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+
+## what it be
+
+My dotfiles. Before you run `install.rb`, make sure you read them beforehand. Maybe it'd even be better to
+just incorporate the parts you like into your own rcfiles.
+
+Dotfiles are prepended with an underscore. `install.rb` will do all the magic.
+
+# files
+
--- install.rb
+++ install.rb
@@ -1,19 +1,26 @@
 #!/usr/bin/env ruby
 
 homedir = ENV["HOME"]
+# get full path to this directory
 thisdir = File.expand_path File.dirname(__FILE__)
+
 Dir.glob("files/_*").each do |file|
   filename = File.basename(file)
+  # get absolute path of $file
   filepath = File.join(thisdir, file)
+  # replace underscore with a dot
   destname = filename.gsub(/(^_)/, ".")
+  # make absolute path for the destination
   destpath = File.join(homedir, destname)
-  if File.symlink?(destpath) then
+  # make sure we're not accidently unlinking an existing rcfile
+  if File.symlink?(destpath) && not File.file?(destpath) then
     puts "path '#{destpath}' is a symlink, will be overwritten"
     File.unlink(destpath)
   elsif File.file?(destpath) then
     puts "destination '#{destpath}' already exists, but it's not a symlink -- refusing to continue!"
     puts "fix this first before running this script again"
   end
+  # then, just symlink it
   puts "symlink(#{filepath.inspect}, #{destpath.inspect})"
   File.symlink(filepath, destpath)
 end
